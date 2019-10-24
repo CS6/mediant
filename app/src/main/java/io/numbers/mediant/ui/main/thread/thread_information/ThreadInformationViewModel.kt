91e12cb34dfe5420441100ba06c6bf8dfaf512cd
Sean Wu <seanwu1105@gmail.com>
2019-10-24 12:45:37 +0800

    Feature: show detailed thread information on fragment.

--- app/src/main/java/io/numbers/mediant/ui/main/thread/thread_information/ThreadInformationViewModel.kt
+++ app/src/main/java/io/numbers/mediant/ui/main/thread/thread_information/ThreadInformationViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.numbers.mediant.api.textile.TextileService
+import io.textile.pb.Model
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -12,20 +13,40 @@ class ThreadInformationViewModel @Inject constructor(private val textileService:
 
     val threadId = MutableLiveData<String>()
     val threadName = MediatorLiveData<String>()
+    val threadKey = MediatorLiveData<String>()
+    val schema = MediatorLiveData<String>()
+    val type = MediatorLiveData<Model.Thread.Type>()
+    val sharingMode = MediatorLiveData<Model.Thread.Sharing>()
     val blockCount = MediatorLiveData<Int>()
+    val headBlockCount = MediatorLiveData<Int>()
+    val peerCount = MediatorLiveData<Int>()
 
     init {
         threadName.addSource(threadId) {
             it?.let { threadName.value = textileService.getThread(it).name }
         }
+        threadKey.addSource(threadId) {
+            it?.let { threadKey.value = textileService.getThread(it).key }
+        }
+        schema.addSource(threadId) {
+            it?.let { schema.value = textileService.getThread(it).schema }
+        }
+        type.addSource(threadId) {
+            it?.let { type.value = textileService.getThread(it).type }
+        }
+        sharingMode.addSource(threadId) {
+            it?.let { sharingMode.value = textileService.getThread(it).sharing }
+        }
         blockCount.addSource(threadId) {
             it?.let { blockCount.value = textileService.getThread(it).blockCount }
+        }
+        headBlockCount.addSource(threadId) {
+            it?.let { headBlockCount.value = textileService.getThread(it).headBlocksCount }
+        }
+        peerCount.addSource(threadId) {
+            it?.let { peerCount.value = textileService.getThread(it).peerCount }
         }
     }
 
     fun changeName() = Timber.d("change name")
-
-    fun inviteOthers() = Timber.d("invite others")
-
-    fun leaveThread() = Timber.d("leave thread")
 }
